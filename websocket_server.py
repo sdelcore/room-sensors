936f9d7e6cf386a8ef4bd2bab5b76b9337177418
Spencer Delcore <sdelcore@uwaterloo.ca>
2017-04-16 14:54:52 -0400

    on connection send data for today through websocket

--- websocket_server.py
+++ websocket_server.py
@@ -1,22 +1,27 @@
 from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+from room_sensors import Room_Sensors
 
-class Sensors:
+class WS_Server:
+  room = Room_Sensors()
+
 	def handleMessage(self):
        for client in clients:
           if client != self:
-             client.sendMessage(self.address[0] + u' - ' + self.data)
+            client.sendMessage(self.data)
 
   def handleConnected(self):
-     print(self.address, 'connected')
-     for client in clients:
-        client.sendMessage(self.address[0] + u' - connected')
-     clients.append(self)
+    print(self.address, 'connected')
+    date = datetime.datetime.now()
+    month = date.strftime("%m")
+    day = date.strftime("%d")
+    year = date.strftime("%Y")
+    for client in clients:
+      client.sendMessage(JSON.dumps(room.getReadingsFromDB(room.sensors.values(), day, month, year)))
+    clients.append(self)
 
   def handleClose(self):
      clients.remove(self)
      print(self.address, 'closed')
-     for client in clients:
-        client.sendMessage(self.address[0] + u' - disconnected')
 
-server = SimpleWebSocketServer('', 8000, Sensors)
+server = SimpleWebSocketServer('', 8000, WS_Server)
 server.serveforever()
