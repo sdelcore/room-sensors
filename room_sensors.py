89ea65793685b030180d93c087502ca0ae7b7d33
Spencer Delcore <sdelcore@uwaterloo.ca>
2017-04-19 22:15:35 -0400

    fixed serial reading

--- room_sensors.py
+++ room_sensors.py
@@ -1,32 +1,35 @@
 from database import Database
+import serial
 
 class Room_Sensors:
 	sensors = {
 	 'DHT': 'dht11_temperature',
-	 'DHH': 'dht_humidity,'
+	 'DHH': 'dht11_humidity',
 	 'SND': 'sound',
 	 'LIT': 'light',
 	 'TMP': 'temperature'
 	}
 
 	def __init__(self):
-		self.arduino = serial.Serial('COM1', 9600, timeout=.1)
+		self.arduino = serial.Serial('/dev/ttyUSB0', 9600)
 		self.database = Database()
 
 	def readSerial(self):
 		readings = []
-		data = self.arduino.readline()
-		while(data.contains('>')):
-			start = data.indexOf('<') + 1
-			end = data.indexOf('>') + 1
+		data = self.arduino.readline().strip()
+		
+		while '<' in data:
+			start = data.find('<') + 1
+			end = data.find('>')
 			reading = data[start:end]
-			data = reading[end:]
-			readings.Add(
+			data = data[end + 1:]
+			readings.append(
 				{
-					'sensor': self.sensors[reading[:2]],
-					'value': float(reading[2:9]),
-					'unit': reading[9:10]
+					'sensor': self.sensors[reading[:3]],
+					'value': float(reading[4:]),
+					'unit': reading[3:4]
 				})
+			
 		return readings
 
 	def sendSerial(self, message):
@@ -38,19 +41,20 @@ class Room_Sensors:
 		day = date.strftime("%d")
 		year = date.strftime("%Y")
 		time = date.strftime("%X")
+
 		for reading in readings:
 			query = "INSERT INTO ? (`value`, `unit`, 'day', 'month', 'year', 'time') VALUES (?, ?, ?, ?, ?, ?)", (reading['sensor'], reading['value'], reading['unit'], day, month, year, time)
 			self.database.insert(query)
 
-	def getReadingsFromDB(self, sensors = self.sensors.values(), day='*', month='*', year='*')
+	def getReadingsFromDB(self, sensors = sensors.values(), day='*', month='*', year='*'):
 		sensor_readings = []
 		for sensor in sensors:
 			select_query = "SELECT * FROM ? WHERE day = ? AND month = ? AND year = ?", (sensor, day, month, year)
-			sensor_readings.Add('sensor': self.database.query(select_query))
+			sensor_readings['sensor'] = self.database.query(select_query)
 		return sensor_readings
 
 if __name__ == "__main__":
 	room = Room_Sensors()
-	room.sendSerial("read")
+	room.sendSerial("1")
 	readings = room.readSerial()
 	room.saveSerial(readings)
