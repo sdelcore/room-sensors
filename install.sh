2dafc3149eeea43374a7c2af67baf07be9da9116
Spencer Delcore <sdelcore@uwaterloo.ca>
2017-04-25 23:22:48 -0400

    added prints

--- install.sh
+++ install.sh
@@ -11,25 +11,31 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+echo "Setting up project directory..."
 if [ ! -d "$proj_dir" ]; then
   mkdir $proj_dir
 else 
   rm -r $proj_dir
 fi
 
-git clone https://github.com/sdelcore/room-sensors.git $proj_dir > /dev/null
-
+echo "Cloning git repo.."
+git clone https://github.com/sdelcore/room-sensors.git $proj_dir
 chmod +x $proj_dir/room_sensors.py
 chmod +x $proj_dir/websocket_server.py
+echo "done."
 
+echo "Creating cron job..."
 echo "0 * * * *	$user $proj_dir/room_sensors.py > /dev/null &" > /etc/cron.d/room_sensors
 echo "@reboot $user $proj_dir/room-sensors/websocket_server.py > /dev/null &" > /etc/cron.d/websocket_server
+echo "done."
 
+echo "Setting up database..."
 mysql --user="$mysql_user" --password="$password" --database="$database" --execute="DROP DATABASE $database; CREATE DATABASE $database;"
 
 for i in "${sensors[@]}"
 do
 	mysql --user="$mysql_user" --password="$password" --execute="use $database; CREATE TABLE $i (id INT NOT NULL AUTO_INCREMENT,value INT,unit VARCHAR(100),date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,PRIMARY KEY ( id ) )"
 done
+echo "done."
 
 $proj_dir/websocket_server.py > /dev/null &
