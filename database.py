4368eb40f0d9f79a30fec58617cb6784a3a75ca2
Spencer Delcore <sdelcore@uwaterloo.ca>
2017-04-25 23:05:08 -0400

    create install script

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python    
 import MySQLdb
 
 class Database:
@@ -5,28 +6,13 @@ class Database:
   user = 'root'
   password = 'asd'
   db = 'sensors'
-  
+
   def __init__(self):
-    sensors = ['dht11_temperature','dht11_humidity','sound','light','temperature']
     self.connection = MySQLdb.connect(self.host, self.user, self.password, self.db)
     self.cursor = self.connection.cursor()
-  	
-  	for sensor in sensors:
-      try:
-      	q ='''CREATE TABLE {0} (
-        	    id INT NOT NULL AUTO_INCREMENT,
-        	    value INT, 
-         	    unit VARCHAR(100), 
-        	    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        	    PRIMARY KEY ( id ) )
-              '''.format(sensor)
-      	self.cursor.execute(q)
-      	self.connection.commit()
-      except:
-      	self.connection.rollback()
 
   def insert(self, query):
-  	try:
+    try:
       self.cursor.execute(query)
       self.connection.commit()
     except:
@@ -40,3 +26,4 @@ class Database:
 
   def __del__(self):
     self.connection.close()
+
---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+user=pi
+mysql_user=root
+password=asd
+database=sensors
+sensors=(dht11_temperature dht11_humidity sound light temperature)
+proj_dir=/home/$user/room-sensors
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+if [ ! -d "$proj_dir" ]; then
+  mkdir $proj_dir
+else 
+  rm -r $proj_dir
+fi
+
+git clone https://github.com/sdelcore/room-sensors.git $proj_dir
+
+chmod +x $proj_dir/room_sensors.py
+chmod +x $proj_dir/websocket_server.py
+
+echo "0 * * * *	$user $proj_dir/room_sensors.py > /dev/null &" > /etc/cron.d/room_sensors
+echo "@reboot $user $proj_dir/room-sensors/websocket_server.py > /dev/null &" > /etc/cron.d/websocket_server
+
+mysql --user="$mysql_user" --password="$password" --database="$database" --execute="DROP DATABASE $database; CREATE DATABASE $database;"
+
+for i in "${sensors[@]}"
+do
+	mysql --user="$mysql_user" --password="$password" --execute="use $database; CREATE TABLE $i (id INT NOT NULL AUTO_INCREMENT,value INT,unit VARCHAR(100),date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,PRIMARY KEY ( id ) )"
+done
+
+$proj_dir/websocket_server.py > /dev/null &
